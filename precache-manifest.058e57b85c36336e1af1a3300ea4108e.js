17b3a2cd51b23b9b3b3f3e3a8f40a14db7a3494a
radhikatekade <tekade.radhika@gmail.com>
2022-08-14 19:44:55 -0700

    Updates

---
+++ precache-manifest.058e57b85c36336e1af1a3300ea4108e.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0516fe6ba750ce515b939df50fe344be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf3a065b18abbe3a96f",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "6f3e6fd8f7a9ff78ea67",
+    "url": "/static/css/main.e0864917.chunk.css"
+  },
+  {
+    "revision": "7dba7cb646e75d6868c1",
+    "url": "/static/js/2.7c056ba8.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.7c056ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf3a065b18abbe3a96f",
+    "url": "/static/js/3.8579ef03.chunk.js"
+  },
+  {
+    "revision": "6f3e6fd8f7a9ff78ea67",
+    "url": "/static/js/main.398b2f00.chunk.js"
+  },
+  {
+    "revision": "4cc404725fd5ec77ffbf",
+    "url": "/static/js/runtime-main.44951c96.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f03ae8bcb72d53af17fde456ea167bd5",
+    "url": "/static/media/aiGirl.f03ae8bc.svg"
+  },
+  {
+    "revision": "5c3e76552b63fa17d605fa538958ecb5",
+    "url": "/static/media/anysysLogo.5c3e7655.svg"
+  },
+  {
+    "revision": "23729096fc927a497ce75f9991ec5048",
+    "url": "/static/media/armManipulator.23729096.jpg"
+  },
+  {
+    "revision": "a6fa766f3690805dc6bdcdb5ec6eefb4",
+    "url": "/static/media/astralLogo.a6fa766f.png"
+  },
+  {
+    "revision": "eadcc38c79c81662adb5969357562ab4",
+    "url": "/static/media/bitsPilani.eadcc38c.jpg"
+  },
+  {
+    "revision": "22913a2df6522f4df6a121b93a66eb7d",
+    "url": "/static/media/carSeat.22913a2d.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "f7bc39bd7693193a93eb49ebe0ce938e",
+    "url": "/static/media/f1.f7bc39bd.png"
+  },
+  {
+    "revision": "411aee4ee12cb8f058bd9b8e0e541911",
+    "url": "/static/media/kthlogo.411aee4e.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "8fed202f4e28af459c01cf96465cbb1d",
+    "url": "/static/media/matlab.8fed202f.svg"
+  },
+  {
+    "revision": "d85933ee705c361e60a32ae378444da0",
+    "url": "/static/media/mslogo.d85933ee.jpg"
+  },
+  {
+    "revision": "799541ffd229e9bf44a9cbc98af30fac",
+    "url": "/static/media/osulogo.799541ff.png"
+  },
+  {
+    "revision": "8382668e6a4a7a88437b77ff1896f3dd",
+    "url": "/static/media/sentimentAnalysis.8382668e.png"
+  },
+  {
+    "revision": "7d34d9701e3ca4ef37cdd77b17e97be8",
+    "url": "/static/media/simulink.7d34d970.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "a6d59eaa4feacb66ca6216e292b96647",
+    "url": "/static/media/solidworksLogo.a6d59eaa.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "20760b3468c3fc9fd52ebd79b3cb2148",
+    "url": "/static/media/techniplogo.20760b34.webp"
+  },
+  {
+    "revision": "d1f556d07193072208ff2e1db7abcacf",
+    "url": "/static/media/turtlebot_rider.d1f556d0.jpg"
+  },
+  {
+    "revision": "c6851b213c290c24a97e78ff161a2fdc",
+    "url": "/static/media/vnitlogo.c6851b21.png"
+  }
+]);
